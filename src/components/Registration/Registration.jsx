e16b25b3f1931bccdc0c55eabbe70067710945a9
SanduBatrincea <alestroc@gmail.com>
2025-12-18 16:04:51 +0100

    navigazione login/registra a MainMenu

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -3,76 +3,96 @@
 import { useState } from "react";
 
 const Registration = ({
-    inputName = "Username", // defaul cambiabile liv. visivo
-    inputPassword = "Password", // defaut cambiabile liv. visivo
-    inputPasswordReconfirm = "Rewrite Password", // default cambiabile liv. visivo
-    urlFetch,
-    isSuccess
+  inputName = "Username", // defaul cambiabile liv. visivo
+  inputPassword = "Password", // defaut cambiabile liv. visivo
+  inputPasswordReconfirm = "Rewrite Password", // default cambiabile liv. visivo
+  urlFetch,
+  isSuccess,
+  setLoginValue,
 }) => {
+  const [messageError, setMessageError] = useState(null); // stato per messaggio errore
 
-    const [messageError, setMessageError] = useState(null) // stato per messaggio errore
-
-    const handleSubmit = (event) => {
-/*  VERIFICA DEI DATI SE PRESENTI PER ESSERE SPEDITI:
+  const handleSubmit = (event) => {
+    /*  VERIFICA DEI DATI SE PRESENTI PER ESSERE SPEDITI:
     event.preventDefault() // blocca l’invio “classico” del form
     console.log(event.target.elements.username.value)
     console.log(event.target.elements.password.value) 
 */
     event.preventDefault(); // qui blocco per inviare i dati nel formato json con fetch
 
-    setMessageError(null) // vado così a cancellare possibili errori se c'è ne sono stati a una richiesta di registrazione precedente rimpostanto lo stato a null
+    setMessageError(null); // vado così a cancellare possibili errori se c'è ne sono stati a una richiesta di registrazione precedente rimpostanto lo stato a null
 
-    const username = event.target.elements.username.value // prendo i valori dell' username
-    const password = event.target.elements.password.value // prendo i valori della password
-    const rewritePassword = event.target.elements.passwordReconfirm.value // prendo i valori della seconda password
+    const username = event.target.elements.username.value; // prendo i valori dell' username
+    const password = event.target.elements.password.value; // prendo i valori della password
+    const rewritePassword = event.target.elements.passwordReconfirm.value; // prendo i valori della seconda password
 
     /* Faccio la richiesta al server tramite Fetch */
     fetch(urlFetch, {
-        method: 'POST', // il metodo è post
-        headers: {'Content-Type':'application/json'}, // gli dico quali dati sta ricevendo in questo caso json
-        body: JSON.stringify({username: username, password: password}) // inserico i dati che voglio spedirgli in json trasformati in stringa da stringify
+      method: "POST", // il metodo è post
+      headers: { "Content-Type": "application/json" }, // gli dico quali dati sta ricevendo in questo caso json
+      body: JSON.stringify({ username: username, password: password }), // inserico i dati che voglio spedirgli in json trasformati in stringa da stringify
     })
-    .then(reply => reply.json()) // ricevo la risposta dal server e converto i dati in json
-    .then((dates) => {
-        if (dates) { // se esistono i dati
-                /* Controllo delle due password: */
-            if (password !== rewritePassword) {
-                    setMessageError("Le password non coincidono!")
-            }else{
-                isSuccess() // esegui la funzione passata dal padre
-            }
-            
+      .then((reply) => reply.json()) // ricevo la risposta dal server e converto i dati in json
+      .then((dates) => {
+        if (dates) {
+          // se esistono i dati
+          /* Controllo delle due password: */
+          if (password !== rewritePassword) {
+            setMessageError("Le password non coincidono!");
+          } else {
+            isSuccess(); // esegui la funzione passata dal padre
+          }
         } else {
-            setMessageError("Registrazione non riuscita")
+          setMessageError("Registrazione non riuscita");
         }
-    }) 
-
-
-
+      });
   };
 
-    return (
-        <div className="page-wrapper">
-            <div className="registration-card">
-                <h1 className="title">REGISTRATION</h1>
-                
-                <form onSubmit={handleSubmit} className="registration-form">
+  return (
+    <div className="page-wrapper">
+      <div className="registration-card">
+        <h1 className="title">REGISTRATION</h1>
 
-                    <label htmlFor="username">{inputName}:</label>
-                    <input id="username" name="username" type="text" placeholder="Write your username" required/>
+        <form onSubmit={handleSubmit} className="registration-form">
+          <label htmlFor="username">{inputName}:</label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            placeholder="Write your username"
+            required
+          />
 
-                    <label htmlFor="password">{inputPassword}:</label>
-                    <input id="password" name="password" type="password" placeholder="Write your password" required/>
+          <label htmlFor="password">{inputPassword}:</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Write your password"
+            required
+          />
 
-                    <label htmlFor="passwordReconfirm">{inputPasswordReconfirm}:</label>
-                    <input id="passwordReconfirm" name="passwordReconfirm" type="password" placeholder="Rewrite your password " required/>
+          <label htmlFor="passwordReconfirm">{inputPasswordReconfirm}:</label>
+          <input
+            id="passwordReconfirm"
+            name="passwordReconfirm"
+            type="password"
+            placeholder="Rewrite your password "
+            required
+          />
 
-                    <button type="submit" className="registration-btn">CREATE ACCOUNT</button>
-
-                </form>
-            </div>
-        </div>
-    )
-}
+          <button
+            className="registration-btn"
+            onClick={(e) => {
+              setLoginValue(!e);
+            }}
+          >
+            CREATE ACCOUNT
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Registration;
