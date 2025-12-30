bdb737c231a6f361737f21f94c644e1d376fe458
CatalinG <catalin.groppo@outlook.com>
2025-12-30 09:19:01 +0100

    Nuovi aggiornamenti con miglioramenti nel codice

--- src/components/EndScreen.jsx
+++ src/components/EndScreen.jsx
@@ -1,72 +1,94 @@
-function EndScreen({ gameWon, gameOverReason, guessesCount, secretCode, colors, onReset }) {
-  const title = gameWon ? '💣 BOMBA 💣 DISINNESCATA! ' : '💥 ESPLOSIONE! 💥';
-  const titleColor = gameWon ? '#10b981' : '#ef4444';
-  const message = gameWon
-    ? `In ${guessesCount} tentativi`
-    : gameOverReason === 'timer'
-      ? 'Tempo scaduto!'
-      : 'Hai usato tutti i tentativi.';
+  import { useState ,useEffect } from "react";
 
-  // Mappa indici colors → veri colori
-  const secretColors = secretCode.map(index => colors[index]);
+  function EndScreen({ gameWon, gameOverReason, guessesCount, secretCode, colors, onReset }) {
 
-  return (
-    <div className="board-bomb" style={{ padding: '40px 36px' }}>
+    const [showSecretCode, setShowSecretCode] = useState(false)
 
-            <h2
-        style={{
-          color: titleColor,
-          fontFamily: 'Orbitron',
-          fontSize: '28px',
-          textAlign: 'center',
-          marginBottom: '20px'
-        }}
-      >
-        {title}
-      </h2>
+    useEffect(() => { 
+      const timer = setTimeout(() => {
+        setShowSecretCode(true);
+      }, 1500);
 
-              <div
-        style={{
-          color: '#eab308',
-          textAlign: 'center',
-          fontSize: '14px',
-          fontFamily: 'Orbitron, monospace',
-          margin: "40px 0 8px 0"
-        }}
-      >
-        Codice segreto:
-      </div>
+      return () => clearTimeout(timer);
+    }, []);
 
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(4, 64px)',
-          gap: '16px',
-          justifyContent: 'center',
-          margin: '20px 0',
-          padding: '12px 0'
-        }}
-      >
-        {secretColors.map((color, index) => (
+    const title = gameWon ? '💣 BOMBA 💣 DISINNESCATA! ' : '💥 ESPLOSIONE! 💥';
+    const titleColor = gameWon ? '#10b981' : '#ef4444';
+    const message = gameWon
+      ? `In ${guessesCount} tentativi`
+      : gameOverReason === 'timer'
+        ? 'Tempo scaduto!'    
+        : 'Hai usato tutti i tentativi disponibili.';
+
+    
+    const secretColors = secretCode.map(index => colors[index]);
+
+    return (
+      <div className="board-bomb" style={{ padding: '40px 36px' }}>
+
+        <h2
+          style={{
+            color: titleColor,
+            fontFamily: 'Orbitron',
+            fontSize: '28px',
+            textAlign: 'center',
+            marginBottom: '20px'
+          }}
+        >
+          {title}
+        </h2>
+
+        <p style={{fontFamily: 'Orbitron', color: titleColor, textAlign: 'center'}}>
+          {message}
+        </p>
+
+        <div style={{
+          opacity: showSecretCode ? 1 : 0,
+          transition: "opacity 2 ease"
+        }}>
           <div
-            key={index}
-            className="peg-bomb"
             style={{
-              backgroundColor: color,
-              border: '3px solid rgba(255,255,255,0.5)',
-              boxShadow: 
-                '0 12px 32px rgba(0,0,0,0.6), ' +
-                '0 0 24px rgba(255,255,255,0.2) inset'
+              color: '#eab308',
+              textAlign: 'center',
+              fontSize: '14px',
+              fontFamily: 'Orbitron, monospace',
+              margin: "40px 0 8px 0"
             }}
-          />
-        ))}
+          >
+            Codice segreto:
+          </div>
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(4, 64px)',
+              gap: '16px',
+              justifyContent: 'center',
+              margin: '20px 0',
+              padding: '12px 0'
+            }}
+          >
+            {secretColors.map((color, index) => (
+              <div
+                key={index}
+                className="peg-bomb"
+                style={{
+                  backgroundColor: color,
+                  border: '3px solid rgba(255,255,255,0.5)',
+                  boxShadow:
+                    '0 12px 32px rgba(0,0,0,0.6), ' +
+                    '0 0 24px rgba(255,255,255,0.2) inset',
+                  transition: ""
+                }}
+              />
+            ))}
+          </div>
+        </div>
+
+        <button className="defuse-btn" onClick={onReset} style={{ marginTop: '24px' }}>
+          NUOVA MISSIONE
+        </button>
       </div>
+    );
+  }
 
-      <button className="defuse-btn" onClick={onReset} style={{ marginTop: '24px' }}>
-        NUOVA MISSIONE
-      </button>
-    </div>
-  );
-}
-
-export default EndScreen
+  export default EndScreen
